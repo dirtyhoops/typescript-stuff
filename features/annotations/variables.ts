ab0e891186d5c24ba234499c6febd5a67add8787
Daryll <dosis@csumb.edu>
2023-03-16 16:07:55 -0700

    initial commit

---
+++ features/annotations/variables.ts
@@ -0,0 +1,28 @@
+const apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,3,4];
+let truth: boolean[] = [true, false, false, true];
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 20
+}
+
+// Function 
